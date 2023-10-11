aef0cc51d51731a3953eae6c93bcec0937fe4460
ithakkar123 <143731880+ithakkar123@users.noreply.github.com>
2023-10-11 18:13:28 +0530

    API Automation of GET_Notification_Preferences

---
+++ cypress/tests/api/VHDA/notification_preferences/get_notification_preferences.cy.js
@@ -0,0 +1,31 @@
+import { VhdaApi } from '../../../../support/api_objects/vhda/vhda';
+import { VhdaPayloadGenerator } from '../../../../support/payload_generators/vhda/vhda_payload_generator';
+
+const { faker } = require('@faker-js/faker');
+
+describe('VHDA: GetNotificationPreferences', function () {
+    const vhdaPayloadGenerator = new VhdaPayloadGenerator();
+    const environment = Cypress.env('vhda');
+    const vhdaApi = new VhdaApi();
+    let credentials = vhdaPayloadGenerator.quick_pay(environment.loan_number, environment.zip, environment.ssn);
+    let jwt = null;
+
+    before(() => {
+        vhdaApi.createQuickPayJwt(credentials);
+        cy.get('@jwt').then(content => {
+            jwt = content
+        })
+    })
+
+    it('get notification preferences returns 200 with valid credentials', () => {
+        vhdaApi.getNotificationPreferences(jwt).then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    }); 
+  
+    it('get notification preferences returns 401 with invalid credentials', () => {
+        vhdaApi.getNotificationPreferences(faker.string.uuid).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
+});
