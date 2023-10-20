9a9616a5a50aa160f3cf0d6c63f3bc52b1b9d7c3
Vitor Bondezan <vbondezan@repay.com>
2023-10-20 13:56:10 -0300

    updated to merge

--- cypress/support/payload_generators/service_mac/service_mac_loan_payment_account.js
+++ cypress/support/payload_generators/service_mac/service_mac_loan_payment_account.js
@@ -13,8 +13,7 @@ export class ServiceMacLoanPaymentAccount {
         return jsonData;
     }
 
-    generateAttributes(loan_number) {
-        const amount = faker.number.int({ min: 0.00, max: 5.00 })
+    generateAttributes(amount) {
         let attributes = {
             payment_amount: amount,
             post_date: moment().format('YYYY-MM-DD'),
@@ -39,4 +38,53 @@ export class ServiceMacLoanPaymentAccount {
 
         return attributes;
     }
+
+    generateTransactionFee() {
+        const transaction_fee = faker.number.int({ min: 0, max: 5 });
+        return transaction_fee
+    }
+
+    //TBD
+    generateAmounts() {
+        {
+            transaction_fee = faker.number.int({ min: 0, max: 5 }),
+                late_fees_paid = faker.number.int({ min: 0, max: 5 }),
+                nsf_fees_paid = faker.number.int({ min: 0, max: 5 }),
+                nsf_fees_paid = faker.number.int({ min: 0, max: 5 });
+        }
+        return
+    }
+
+
+    generatelateFeesPaid() {
+        const late_fees_paid = faker.number.int({ min: 0, max: 5 });
+        return late_fees_paid
+    }
+
+    generateNsfFeesPaid() {
+        const nsf_fees_paid = faker.number.int({ min: 0, max: 5 });
+        return nsf_fees_paid
+    }
+
+    generateOtherFeesPaid() {
+        const other_fees_paid = faker.number.int({ min: 0, max: 5 });
+        return other_fees_paid
+    }
+    generatePaymentAmount() {
+        const payment_amount = faker.number.int({ min: 0, max: 5 });
+        return payment_amount
+    }
+    generateSuspense() {
+        const suspense = faker.number.int({ min: 0, max: 5 });
+        return suspense
+    }
+    generateEscrow() {
+        const suspense = faker.number.int({ min: 0, max: 5 });
+        return suspense
+    }
+    generateApplyTowardsprincipal() {
+        const apply_towards_principal = faker.number.int({ min: 0, max: 5 });
+        return apply_towards_principal
+    }
+
 }
--- cypress/tests/api/SERVICE_MAC/post_loan_payment_account.cy.js
+++ cypress/tests/api/SERVICE_MAC/post_loan_payment_account.cy.js
@@ -1,31 +1,30 @@
 import { faker } from "@faker-js/faker";
 import { ServiceMacAccount } from "../../../support/api_objects/ServiceMac/service_mac_account"
 import { ServiceMacLoanPaymentAccount } from "../../../support/payload_generators/service_mac/service_mac_loan_payment_account";
+const moment = require('moment');
 
 describe('API Tests: SERVICE_MAC', function() {
     const serviceMacApi = new ServiceMacAccount();
     const serviceMacPayloadGenerator = new ServiceMacLoanPaymentAccount();
     
-    it('Post Account Holder [200]: post valid account holder',() => {
+    it('Post Loan Payment Account [200]: post valid account holder',() => {
 
-        const loan_number = serviceMacApi.getEnvironment().loan_number
-        let payload = serviceMacPayloadGenerator.generateData(loan_number);
+        let payload = serviceMacPayloadGenerator.generateData();
         cy.log(JSON.stringify(payload))        
 
-        serviceMacApi.postAccountHolder(payload).then((response) => {
+        serviceMacApi.postLoanPaymentAccount(payload).then((response) => {
             
             expect(response.status).to.eq(201);
-            expect(response.body.data.id).to.eq(serviceMacApi.getEnvironment().account);
+            expect(response.body.data.attributes.transaction_date).to.eq(moment().format('YYYY-MM-DD'));
         });
     });
 
-    it('Post Account Holder [404]: post invalid account holder',() => {
-        const account = faker.number.int({ min: 10000000000, max: 99999999999 })
-        const payload = serviceMacPayloadGenerator.generateData(account)
+    it('Post Loan Payment Account [422]: post invalid account holder',() => {
+        const payload = serviceMacPayloadGenerator.generateData()
 
-        serviceMacApi.postAccountHolder(payload).then((response) => {
+        serviceMacApi.postLoanPaymentAccount(payload).then((response) => {
             
-            expect(response.status).to.eq(404);
+            expect(response.status).to.eq(422);
             expect(response.body.errors).to.eq('Loan not found');
         });
     });
