ee6221fd7b4d5082fa7df99dee5f0addd12ed9d1
ithakkar123 <143731880+ithakkar123@users.noreply.github.com>
2024-01-29 18:10:55 +0530

    createloan and get payments

--- cypress/tests/api/VHDA/payment/get_payment.js
+++ cypress/tests/api/VHDA/payment/get_payment.js
@@ -1,5 +1,5 @@
 import { VhdaApi } from '../../../../support/api_objects/vhda/vhda';
-
+const { faker } = require('@faker-js/faker');
 describe('VHDA: GET payment', function () {
     const vhdaApi = new VhdaApi();
     const credentials = vhdaApi.payloadGenerator.quick_pay(vhdaApi.cypressEnv.loan_number, vhdaApi.cypressEnv.zip, vhdaApi.cypressEnv.ssn);
@@ -7,4 +7,15 @@ describe('VHDA: GET payment', function () {
     before(() => {
         vhdaApi.createQuickPayJwt(credentials);
     })
+    it('get Payment returns 200 with valid credentials', () => {
+        vhdaApi.getPayments().then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.data.id).to.not.equal(null);
+        });
+    });
+    it('get Payment preferences returns 401 with invalid credentials', () => {
+        vhdaApi.getPayments(faker.string.uuid).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
 })
