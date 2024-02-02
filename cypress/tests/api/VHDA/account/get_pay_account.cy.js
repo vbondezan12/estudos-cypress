08922d0f2d1169147492eacce5d16de2e342c5b7
ithakkar123 <143731880+ithakkar123@users.noreply.github.com>
2024-02-02 18:24:44 +0530

    Automation of VHDA Get Pay Accounts API

---
+++ cypress/tests/api/VHDA/account/get_pay_account.cy.js
@@ -0,0 +1,24 @@
+import { VhdaApi } from '../../../../support/api_objects/vhda/vhda';
+const { faker } = require('@faker-js/faker');
+
+describe('VHDA: GetPayAccount', function () {
+    const vhdaApi = new VhdaApi();
+    const credentials = vhdaApi.payloadGenerator.quick_pay(vhdaApi.cypressEnv.loan_number, vhdaApi.cypressEnv.zip, vhdaApi.cypressEnv.ssn);
+
+    before(() => {
+        vhdaApi.createQuickPayJwt(credentials);
+    })
+
+    it('get pay account returns 200 with valid credentials', () => {
+        vhdaApi.getPayAccounts().then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.data.type).to.not.equal(null);
+        });
+    });
+
+    it('get pay account returns 401 with invalid credentials', () => {
+        vhdaApi.getPayAccounts(faker.string.uuid).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
+});
