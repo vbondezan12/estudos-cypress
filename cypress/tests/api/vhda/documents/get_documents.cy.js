f1c8e175c0cd0e4462ecda76fe86fa884160a526
ithakkar123 <143731880+ithakkar123@users.noreply.github.com>
2024-03-21 22:35:37 +0530

    Vhda code refactor getmspotherfees (#46)
    Following file is changed
    - get_msp_other_fees.cy.js

--- cypress/tests/api/vhda/documents/get_documents.cy.js
+++ cypress/tests/api/vhda/documents/get_documents.cy.js
@@ -1,24 +1,32 @@
 import { VhdaApi } from '../../../../support/api_objects/vhda/vhda_api';
+import { LOAN_STATUS } from '../../../../config/constants';
 
 const { faker } = require('@faker-js/faker');
 
 describe('vhda: GetDocuments', function () {
   const vhdaApi = new VhdaApi();
-  const credentials = vhdaApi.payloadGenerator.quickPay(vhdaApi.cypressEnv.loan_number, vhdaApi.cypressEnv.zip,
-    vhdaApi.cypressEnv.ssn);
-
+  let Test_Credential;
   before(() => {
-    vhdaApi.createQuickPayJwt(credentials);
+    const testPayload = vhdaApi.payloadGenerator.generateTestCredentialsLookupPayload(LOAN_STATUS.CURRENT);
+    vhdaApi.getTestLoans(testPayload).then((response) => {
+      Test_Credential = response.body['test_credentials'][0];
+    });
   });
 
   it('get documents returns 200 with valid credentials', () => {
+    const payload = vhdaApi.payloadGenerator.quickPay(Test_Credential.loan_number, Test_Credential.zip_code,
+      Test_Credential.last_4_ssn);
+    vhdaApi.createQuickPayJwt(payload);
     vhdaApi.getDocuments().then((response) => {
       expect(response.status).to.eq(200);
     });
   });
 
   it('get documents returns 401 with invalid credentials', () => {
-    vhdaApi.getDocuments(faker.string.uuid).then((response) => {
+    const Credentials = vhdaApi.payloadGenerator.quickPay(faker.finance.accountNumber(8),
+      faker.number.int(5), faker.number.int(4));
+    vhdaApi.createQuickPayJwt(Credentials);
+    vhdaApi.getDocuments().then((response) => {
       expect(response.status).to.eq(401);
     });
   });
--- cypress/tests/api/vhda/get_msp_other_fees/get_msp_other_fees.cy.js
+++ cypress/tests/api/vhda/get_msp_other_fees/get_msp_other_fees.cy.js
@@ -1,20 +1,22 @@
 import { VhdaApi } from '../../../../support/api_objects/vhda/vhda_api';
+import { LOAN_STATUS } from '../../../../config/constants';
 
 const { faker } = require('@faker-js/faker');
 
 describe('vhda: GetMSPOtherFees', function () {
   const vhdaApi = new VhdaApi();
-  let credentials = vhdaApi.payloadGenerator.quickPay(
-    vhdaApi.cypressEnv.loan_number,
-    vhdaApi.cypressEnv.zip,
-    vhdaApi.cypressEnv.ssn
-  );
-
+  let Test_Credential;
   before(() => {
-    vhdaApi.createQuickPayJwt(credentials);
+    const testPayload = vhdaApi.payloadGenerator.generateTestCredentialsLookupPayload(LOAN_STATUS.CURRENT);
+    vhdaApi.getTestLoans(testPayload).then((response) => {
+      Test_Credential = response.body['test_credentials'][0];
+    });
   });
 
   it('get MSP other Fees returns 200 with valid credentials', () => {
+    const payload = vhdaApi.payloadGenerator.quickPay(Test_Credential.loan_number, Test_Credential.zip_code,
+      Test_Credential.last_4_ssn);
+    vhdaApi.createQuickPayJwt(payload);
     vhdaApi.getMspOtherFees().then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.not.equal(null);
@@ -22,7 +24,10 @@ describe('vhda: GetMSPOtherFees', function () {
   });
 
   it('get MSP other Fees returns 401 with invalid credentials', () => {
-    vhdaApi.getMspOtherFees(faker.string.uuid).then((response) => {
+    const Credentials = vhdaApi.payloadGenerator.quickPay(faker.finance.accountNumber(8),
+      faker.number.int(5), faker.number.int(4));
+    vhdaApi.createQuickPayJwt(Credentials);
+    vhdaApi.getMspOtherFees().then((response) => {
       expect(response.status).to.eq(401);
     });
   });
