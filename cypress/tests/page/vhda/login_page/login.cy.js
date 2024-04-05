fce0fecd9be433161d9fa6e1d56f7b6a3e7ce555
PriscillaRepay <pdesouzasilva@repay.com>
2024-04-05 18:38:29 +0200

    commite 5

--- cypress/tests/page/vhda/login_page/login.cy.js
+++ cypress/tests/page/vhda/login_page/login.cy.js
@@ -1,6 +1,6 @@
 import { LoginPage } from '../../../../support/page_objects/vhda/login/login_page';
 
 describe('Login Priscilla', { tags: [ '@Login', '@regression' ] }, () => {
-    let loginPage = new LoginPage()
+    let loginPage = new LoginPage();
 
 })
