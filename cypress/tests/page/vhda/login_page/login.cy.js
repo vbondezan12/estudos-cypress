34b5f4dd717bb036e4ce17d93dcd37b427054ab5
PriscillaRepay <pdesouzasilva@repay.com>
2024-04-05 15:44:06 +0200

    commit

--- cypress/tests/page/vhda/login_page/login.cy.js
+++ cypress/tests/page/vhda/login_page/login.cy.js
@@ -1,1 +1,21 @@
 import {LoginPage} from '../../../../support/page_objects/vhda/login/login_page'
+
+describe('Tests of Login', { tags: [ '@Login', '@regression' ] }, () => {
+    let loginPage = new LoginPage()
+     
+    it('Login successfully with valid credentials', { tags: '@smoke' }, function () {
+
+        //Variables necessary to execute the tests cases
+        const userName = 'PriscillaRepay';
+        const password = 'Repay123Repay123@';
+
+        //Test execution
+        loginPage.openPage();   
+        loginPage.enterUserName(userName);
+        loginPage.enterPassword(password);  
+
+        //Test validation
+        cy.url().should('contains', `${ Cypress.config().vhda.baseUrl }/multifactor/new`);
+
+      });
+      });
