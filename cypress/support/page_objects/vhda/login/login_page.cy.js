e9568465209539b03643655f8d93d6209c1389e8
PriscillaRepay <pdesouzasilva@repay.com>
2024-04-05 15:00:55 +0200

    commit login one

--- cypress/support/page_objects/vhda/login/login_page.cy.js
+++ cypress/support/page_objects/vhda/login/login_page.cy.js
@@ -1,2 +1,52 @@
 const url = `${ Cypress.config().vhda.baseUrl }/login`;
 
+export class LoginPage{
+
+    open() {
+        return cy.visit(url);
+      }
+    get userNameInput() {
+        return cy.get('#user_username');
+      }   
+    get passwordInput() {
+        return cy.get('#user_password');
+      } 
+      
+    get loginButton() {
+        return cy.get('.btn');
+      } 
+
+    get toastMessage() {
+        return cy.get('.toast-message');
+      }
+
+    get errorMessage() {
+        return cy.get('.card-body');
+      }
+
+    login(userName,password){
+      this.open();
+      enterUserName(userName);
+      enterPssword(password)
+      this.clickLoginButton();
+    }
+
+      openPage(){
+        this.open();
+      }
+
+     enterUserName(userName){
+      this.userNameInput.type(userName)
+     }
+  
+     enterPassword(password){
+      this.passwordInput.type(password)
+     }
+  
+     clickLoginButton(){
+      this.loginButton.click();
+     }
+
+    }
+
+
